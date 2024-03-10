834640fdc429b28b42db4ac87c7fba8d0b1219d1
hundred215 <hundred215@gmail.com>
2024-03-10 15:05:57 +0900

    Initial commit

---
+++ app/src/main/java/com/example/draw_hackers/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.draw_hackers
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.viewModels
+import com.example.draw_hackers.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+    }
+}
